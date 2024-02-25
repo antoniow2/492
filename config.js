be47e62fc8794cca0a50ee29dc74f23d6392ab3e
Antonio Turner <antoniow@email.sc.edu>
2024-02-24 20:35:10 -0500

    6

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   development: {
+    connectionLimit: 100,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
